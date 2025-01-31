2c5048d000d135993f19db9149da5acf2ab128c7
magnof <70082138+chaknogf@users.noreply.github.com>
2025-01-31 15:39:53 -0600

    feat: duckdns

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: 4800
+    port: 4800,
+    allowedHosts: ['hgtecpan.duckdns.org', 'localhost', '127.0.0.1'],
   }
 })
